1ce7cf70a0a2f20469f18494da98db70dd20b7e4
Stevan Bogosavljević <stevan_nk@ymail.com>
2014-10-05 22:29:30 +0200

    Initial commit

---
+++ android/src/me/montecode/game/gdx/threetiles/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package me.montecode.game.gdx.threetiles.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import me.montecode.game.gdx.threetiles.ThreeTilesGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ThreeTilesGame(), config);
+	}
+}
---
+++ desktop/src/me/montecode/game/gdx/threetiles/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.montecode.game.gdx.threetiles.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.montecode.game.gdx.threetiles.ThreeTilesGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ThreeTilesGame(), config);
+	}
+}
