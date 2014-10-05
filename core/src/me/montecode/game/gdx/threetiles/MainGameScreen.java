edb4b0b60529a616b29c8ee21eec1383ee1a5c85
Stevan Bogosavljević <stevan_nk@ymail.com>
2014-10-05 23:04:27 +0200

    Added MainGameScreen

---
+++ core/src/me/montecode/game/gdx/threetiles/MainGameScreen.java
@@ -0,0 +1,50 @@
+package me.montecode.game.gdx.threetiles;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by stevyhacker on 5.10.14..
+ */
+public class MainGameScreen implements Screen {
+   ThreeTilesGame game;
+
+
+    public MainGameScreen(ThreeTilesGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
